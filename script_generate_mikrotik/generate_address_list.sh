778ca6217f4b2d74c9dcb469099385aa5d434266
Alexandr <zloybabay666@gmail.com>
2024-08-27 16:56:30 +0300

    Create generate_address_list.sh

---
+++ script_generate_mikrotik/generate_address_list.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Путь к вашему файлу с URL
+input_file="youtube-url.txt"
+
+# Путь к выходному файлу .rsc
+output_file="youtube-url.rsc"
+
+# Название адресного списка в MikroTik
+address_list_name="YouTube-URL"
+
+# Создание или очистка выходного файла
+> "$output_file"
+
+# Записываем команды для создания или очистки адресного списка
+echo "/ip firewall address-list remove [find list=\"$address_list_name\"]" >> "$output_file"
+echo "/ip firewall address-list add list=\"$address_list_name\" address=0.0.0.0 comment=\"Placeholder entry\"" >> "$output_file"
+
+# Записываем команды для добавления доменов в адресный список
+while IFS= read -r url; do
+    if [[ -n "$url" ]]; then
+        echo "/ip firewall address-list add list=\"$address_list_name\" address=\"$url\"" >> "$output_file"
+    fi
+done < "$input_file"
+
+echo "Successfully created $output_file with commands."
+
